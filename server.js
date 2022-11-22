866b7a8cba274d046979e07275dd6a2a8eeb6569
RobertHJones <roberthuwjones@outlook.com>
2022-11-22 11:35:39 +0000

    data

--- server.js
+++ server.js
@@ -6,13 +6,54 @@ import { makeExecutableSchema } from "@graphql-tools/schema";
 const app = express();
 const port = 4000;
 
+// data
+const data = {
+  players: [
+    { id: "001", name: "Jude" },
+    { id: "002", name: "Bukayo" },
+  ],
+};
+
+// schema
+const typeDefs = `
+type Player {
+  id: ID!
+  name: String!
+}
+
+type Query {
+  players: [Player]
+}
+`;
+
+// resolver
+const resolvers = {
+  Query: {
+    players: (obj, args, context, info) => context.players,
+  },
+};
+
+const executableSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (request, response) => {
-  response.send("Hello, GraphQL!");
-});
+// app.get("/", (request, response) => {
+//   response.send("Hello, GraphQL!");
+// });
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: executableSchema,
+    context: data,
+    graphiql: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Running a server at http://localhost:${port}`);
